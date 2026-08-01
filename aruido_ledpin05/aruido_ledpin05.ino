98010f52ca52e6535efb2ea8c61a15c99216cd14
nry422 <henrymuller@gmail.com>
2026-08-01 17:49:21 -0300

    FOR

--- aruido_ledpin05/aruido_ledpin05.ino
+++ aruido_ledpin05/aruido_ledpin05.ino
@@ -1,4 +1,4 @@
-//define LED_PIN 12
+/* /define LED_PIN 12
 int LED_PIN1 = 12;
 int LED_PIN2 = 11;
 int LED_PIN3 = 10;
@@ -6,22 +6,38 @@ int LED_PIN4 = 9;
 int LED_PIN5 = 8;
 int LED_PIN6 = 7;
 int LED_PIN7 = 6;
-int LED_PIN8 = 5;
+int LED_PIN8 = 5; */
+
+int ledpin[] = {12,11,10,9,8,7,6,5};
+int numpin = 8;
 
 
 void setup() {
-  pinMode(LED_PIN1, OUTPUT);  //habilita o pino 12 para saida digital (outpit)
+  /*pinMode(LED_PIN1, OUTPUT);  //habilita o pino 12 para saida digital (outpit)
   pinMode(LED_PIN2, OUTPUT);
   pinMode(LED_PIN3, OUTPUT);
   pinMode(LED_PIN4, OUTPUT);
   pinMode(LED_PIN5, OUTPUT);
   pinMode(LED_PIN6, OUTPUT);
   pinMode(LED_PIN7, OUTPUT);
-  pinMode(LED_PIN8, OUTPUT);
+  pinMode(LED_PIN8, OUTPUT); */
+
+  for (int i=0; i < numpin; i++) {
+
+     pinMode(ledpin[i], OUTPUT); }
+
+
 }
 
 void loop() {
-  digitalWrite(LED_PIN1, HIGH);  //liga o LED
+
+  for (int i=0; i < numpin; i++) {
+    digitalWrite(ledpin[i], HIGH);
+    delay(200);
+    digitalWrite(ledpin[i], LOW);
+    delay(200);
+  }
+  /*digitalWrite(LED_PIN1, HIGH);  //liga o LED
   delay(50);
   digitalWrite(LED_PIN1, LOW);  // desliga o LED
   delay(50);
@@ -52,5 +68,5 @@ void loop() {
   digitalWrite(LED_PIN8, HIGH);  //liga o LED
   delay(50);
   digitalWrite(LED_PIN8, LOW);  // desliga o LED
-  delay(50);
+  delay(50);*/
 }
--- aruido_ledpin06/aruido_ledpin06.ino
+++ aruido_ledpin06/aruido_ledpin06.ino
@@ -1,28 +1,58 @@
 //define LED_PIN 12
-int LED_PIN1 = 12;
+/*int LED_PIN1 = 12;
 int LED_PIN2 = 11;
 int LED_PIN3 = 10;
 int LED_PIN4 = 9;
 int LED_PIN5 = 8;
 int LED_PIN6 = 7;
 int LED_PIN7 = 6;
-int LED_PIN8 = 5;
+int LED_PIN8 = 5;*/
+
+int pins[] = {12, 11, 10, 9, 8, 7, 6, 5};
+int numpin = 8;
 
 
 void setup() {
-  pinMode(LED_PIN1, OUTPUT);  //habilita o pino 12 para saida digital (outpit)
-  pinMode(LED_PIN2, OUTPUT);
-  pinMode(LED_PIN3, OUTPUT);
-  pinMode(LED_PIN4, OUTPUT);
-  pinMode(LED_PIN5, OUTPUT);
-  pinMode(LED_PIN6, OUTPUT);
-  pinMode(LED_PIN7, OUTPUT);
-  pinMode(LED_PIN8, OUTPUT);
+
+  int numpin = 8;
+
+  for (int i=0; i < numpin; i++) {
+
+     pinMode(12-i, OUTPUT);
+
+  }
+
+  /*pinMode(12, OUTPUT);  //habilita o pino 12 para saida digital (outpit)
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT); */
 }
 
 void loop() {
 
-  digitalWrite(LED_PIN1, HIGH);
+   for (int i = 0; i < 4; i++) { //já que ativamos em pares 4 é a metade do total de pinos
+    digitalWrite(pins[i], HIGH);
+    digitalWrite(pins[numpin - 1 - i], HIGH);
+    delay(1000);
+    digitalWrite(pins[i], LOW);
+    digitalWrite(pins[numpin - 1 - i], LOW);
+  }
+
+   for (int i = 2; i >= 0; i--) { // i é 2 pq o final do anterior é 3 (<4) e para não repetir o 3 uso 2
+    digitalWrite(pins[i], HIGH);
+    digitalWrite(pins[numpin - 1 - i], HIGH);
+    delay(1000);
+    digitalWrite(pins[i], LOW);
+    digitalWrite(pins[numpin - 1 - i], LOW);
+  }
+
+  delay(1000); 
+
+  /* digitalWrite(LED_PIN1, HIGH);
   digitalWrite(LED_PIN8, HIGH);
   delay(1000);
 
@@ -65,6 +95,6 @@ void loop() {
 
   digitalWrite(LED_PIN1, LOW);
   digitalWrite(LED_PIN8, LOW);
-  delay(1000);
+  delay(1000); */
 
 }
